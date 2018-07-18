5d2ddd8f6a630a0e2de870be9e0b0ea58f7caa98
peterennis <peterennis@yahoo.com>
2018-07-17 21:27:25 -0700

    chore() fix lint errors

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -16,27 +16,27 @@ export class HomePage {
   doStars() {
     // Ref: http://www.petercollingridge.co.uk/tutorials/svg/animation/starfield/
 
-    let svgDocument = document.getElementById('starfield');
-    let svgNS = "http://www.w3.org/2000/svg";
-    let xlinkNS = "http://www.w3.org/1999/xlink";
+    const svgDocument = document.getElementById('starfield');
+    const svgNS = 'http://www.w3.org/2000/svg';
+    const xlinkNS = 'http://www.w3.org/1999/xlink';
 
-    let angles = [];
-    let distances = [];
-    let nStars = 32;
+    const angles = [];
+    const distances = [];
+    const nStars = 32;
 
-    let centerX = +svgDocument.getAttributeNS(null, 'width') / 2;
-    let centerY = +svgDocument.getAttributeNS(null, 'height') / 2;
-    let maxD = Math.sqrt(centerX * centerX + centerY * centerY) + 40;
+    const centerX = +svgDocument.getAttributeNS(null, 'width') / 2;
+    const centerY = +svgDocument.getAttributeNS(null, 'height') / 2;
+    const maxD = Math.sqrt(centerX * centerX + centerY * centerY) + 40;
 
     createStars(nStars);
     beginAnimation();
 
     function createStars(n) {
       for (let i = 1; i <= n; i++) {
-        let star = document.createElementNS(svgNS, "use");
-        star.setAttributeNS(null, "id", "star" + i);
-        star.setAttributeNS(xlinkNS, "href", "#star");
-        document.getElementById("star-group").appendChild(star);
+        const star = document.createElementNS(svgNS, 'use');
+        star.setAttributeNS(null, 'id', 'star' + i);
+        star.setAttributeNS(xlinkNS, 'href', '#star');
+        document.getElementById('star-group').appendChild(star);
         angles.push(Math.PI * 2 * Math.random());
         distances.push(maxD * Math.random());
       }
@@ -44,11 +44,11 @@ export class HomePage {
 
     function drawField() {
       for (let i = 0; i < nStars; i++) {
-        let star = document.getElementById('star' + (i + 1));
-        let d = distances[i];
-        let x = centerX + d * Math.sin(angles[i]);
-        let y = centerY + d * Math.cos(angles[i]);
-        let scale = 2 * (1 - (maxD / (d + maxD)));
+        const star = document.getElementById('star' + (i + 1));
+        const d = distances[i];
+        const x = centerX + d * Math.sin(angles[i]);
+        const y = centerY + d * Math.cos(angles[i]);
+        const scale = 2 * (1 - (maxD / (d + maxD)));
 
         star.setAttributeNS(null, 'transform', 'translate(' + x + ',' + y + ') scale(' + scale * scale + ')');
       }
@@ -65,10 +65,10 @@ export class HomePage {
     }
 
     function beginAnimation() {
-      let timeout = setInterval(updateImage, 10);
-    }    
+      const timeout = setInterval(updateImage, 10);
+    }
   }
-  
+
   ionViewDidEnter() {
     console.log('ionViewDidEnter');
     this.doStars();
