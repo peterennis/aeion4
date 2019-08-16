cfb669596d7b0917ceafe643cb6894aa2a148c7b
peterennis <peterennis@yahoo.com>
2019-08-15 21:34:35 -0700

    fix(): min max type is number

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -73,7 +73,7 @@ export class HomePage implements OnInit {
     }
   }
 
-  randomIntFromInterval(min, max) { // min and max included 
+  randomIntFromInterval(min: number, max: number) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
